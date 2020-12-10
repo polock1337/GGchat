bee7b22fcc640c7300d2bb9778856193d098a391
Antoine Bélanger <31995891+ThonyDelire@users.noreply.github.com>
2020-12-10 11:26:33 -0500

    Statistique Fonctionnel

--- ggchat_php/classe/view/Statistique.php
+++ ggchat_php/classe/view/Statistique.php
@@ -18,7 +18,7 @@ class Statistique extends Page
   }
     
 
-    public static function listerContenu()
+    public function listerContenu()
     {
         $DbhObject = new Dbh();
         $dbh = $DbhObject->getDbh();
@@ -46,38 +46,39 @@ class Statistique extends Page
           }
         }
 
-        $this->doc.='<a>test</a>';
+        //$this->doc.='<a>test</a>';
 
         /*echo $resultat["groupe_fkey"];
         echo $resultat["nombre"];
-        //$this->doc.='<ul>';
-        $this->doc.='<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
-        <div class="chart-container" style="position: relative; height:20vh; width:40vw;">
-              <canvas id="graphique" ></canvas>
-        </div>
-        
-        <script>
-        var donnees = [<?php echo $nombreGroup1; ?>, <?php echo $nombreGroup2; ?>, <?php echo $nombreGroup3; ?>];
-        var etiquettes = ["japonaise", "française", "allemande"];
-        var couleurs = ["rgba(255, 99, 132, 0.9)","rgba(54, 162, 235, 0.9)", "rgba(255, 206, 86, 0.9)"]
-        
-        var cible = document.getElementById("graphique");
-        var graphiqueTarte = new Chart(cible, {
-            type: "pie",
-            data: {
-                labels: etiquettes,
-                datasets: [{
-                    label: "Contenu par catégorie",
-                    data: donnees,
-                    backgroundColor: couleurs
-                }]
-            },
-            options: {
-                responsive: true
-            }
-        });
-        </script>';
-        //$this->doc.='</ul>';*/
+       $this->doc.='<ul>';*/
+       $this->doc.='<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+       <h1 style="text-align: center; color: white;">Statistique</h1>
+       <h2 style="text-align: center; color: white;">Nombre de messages par groupe de discussion</h2>
+       <div class="chart-container" style="position: relative; height:20vh; width: 100%; text-align:center">
+              <canvas id="graphique" style="display: inline;" ></canvas>
+      </div>
+      <script>
+            var donnees = ['.$nombreGroup1.', '.$nombreGroup2.', '.$nombreGroup3.'];
+            var ctx = document.getElementById("graphique").getContext("2d");
+            var chart = new Chart(ctx, {
+          // The type of chart we want to create
+          type: "doughnut",
+
+          // The data for our dataset
+          data: {
+              labels: ["Groupe Sport", "Groupe informatique", "Groupe General"],
+              datasets: [{
+                  label: "My First dataset",
+                  backgroundColor: ["rgba(4, 199, 82)","rgba(4, 193, 199, 0.9)", "rgba(255, 206, 86, 0.9)"],
+                  borderColor: "rgb(0, 0, 0)",
+                  data: donnees
+              }]
+          },
+
+          // Configuration options go here
+          options: {}
+          });
+          </script>';
 
 
     }
