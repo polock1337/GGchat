175af1bfb93d0d95fa6e2feeeb5ca1b9d5883207
CharlesC1337 <CharlesCarrier1337@hotmail.com>
2020-12-23 01:18:03 -0500

    Ajout d'information dans le readme et ajout de l'export sql de la bd

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-# projet-social-2020-CharlesC1337
+# projet-social-2020-Charles-Antoine-Gabriel-Jacob
 
 Thème de l'application : Application de chat
-
 
 Équipe :
 
@@ -11,6 +10,34 @@ Thème de l'application : Application de chat
 - Jacob
 
 Fiche de remise : https://docs.google.com/presentation/d/1mo011MwXWcd-ysc6iVHE_5Y_kD5_dI9cB26zDSV3Dpo/edit?usp=sharing
+
+## Informations d'installations : 
+
+1. Télécharger le dossier "ggchat_electron4php" présent dans https://github.com/cegepmatane/projet-social-2020-Charles-Antoine-Gabriel-Jacob et le mettre ou vous voulez.
+
+2. Télécharger node.js si pas déjà fait et ouvrir node.js command prompt.
+
+3. Se rendre dans le dossier "ggchat_electron4php" copié précédamment en utilisant les commandes "cd".
+
+4. Faire "npm start" pour démarrer l'application et enjoy! :)
+
+## Informations sur l'application : 
+
+Application fait en php avec electron.
+
+Base de données postgresql sur un serveur vps distant (Celui à antoine).
+
+Pour autres informations voir fiche de remise : https://docs.google.com/presentation/d/1mo011MwXWcd-ysc6iVHE_5Y_kD5_dI9cB26zDSV3Dpo/edit?usp=sharing
+
+## Informations sur la hiéarchie github :
+
+- Le dossier "electron_4_php" est le "template" de electron pour php de base.
+- Le dossier "ggchat_php" est le code php comme tel.
+- Le dossier "ggchat_electron4php" est l'application finale electron.
+- Le dossier Maquette contient les maquettes.
+- Le dossier "OptimisationServeur" contient les optimisations apposées au côté du serveur, donc Charles et Antoine.
+- Le fichier "ggchatlocal.sql" contient l'export sql de la base de donnée fait à la main.
+- Le fichier "ggchat.sql" contient l'export sql de la base de donnée présentement sur le serveur fait à l'aide de la commande "pg_dump".
 
 ## Commande CRON pour optimisation archivage (Charles) : 
 
@@ -40,6 +67,8 @@ ex : 0 0 * * * /usr/local/bin/php /var/scripts/Archivage.php
 
 sudo apt-get install php-redis
 sudo apt install redis-server
-## 2 Crontab
+
+### 2 Crontab
+
 crontab -e
 0 0 * * * /usr/local/bin/php /var/scripts/UpdateListeSalonRedis.php
---
+++ ggchatlocal.sql
@@ -0,0 +1,133 @@
+-- Database: ggchat
+
+-- DROP DATABASE ggchat;
+
+CREATE DATABASE ggchat
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'French_Canada.1252'
+    LC_CTYPE = 'French_Canada.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+	
+-- Table: public.groupe
+
+-- DROP TABLE public.groupe;
+
+CREATE TABLE public.groupe
+(
+    id SERIAL NOT NULL,
+    groupe_nom character varying(50) COLLATE pg_catalog."default",
+    CONSTRAINT groupe_pkey PRIMARY KEY (id)
+)
+
+-- Table: public.membre
+
+-- DROP TABLE public.membre;
+
+CREATE TABLE public.membre
+(
+    id SERIAL NOT NULL,
+    membre_admin boolean,
+    membre_first character varying(50) COLLATE pg_catalog."default",
+    membre_last character varying(50) COLLATE pg_catalog."default",
+    membre_email character varying(100) COLLATE pg_catalog."default",
+    membre_uid character varying(50) COLLATE pg_catalog."default",
+    membre_pwd character varying(262) COLLATE pg_catalog."default",
+    CONSTRAINT membre_pkey PRIMARY KEY (id)
+)
+
+-- Table: public.message_groupe
+
+-- DROP TABLE public.message_groupe;
+
+CREATE TABLE public.message_groupe
+(
+    id SERIAL NOT NULL,
+    groupe_fkey integer,
+    membre_fkey integer,
+    message_groupe_contenu character varying(255) COLLATE pg_catalog."default",
+    "timestamp" character varying(255) COLLATE pg_catalog."default",
+    CONSTRAINT groupe_fkey FOREIGN KEY (groupe_fkey)
+        REFERENCES public.groupe (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT membre_fkey FOREIGN KEY (membre_fkey)
+        REFERENCES public.membre (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+-- Table: public.message_groupe_archive
+
+-- DROP TABLE public.message_groupe_archive;
+
+CREATE TABLE public.message_groupe_archive
+(
+    id SERIAL NOT NULL,
+    groupe_fkey integer,
+    membre_fkey integer,
+    message_groupe_contenu character varying(255) COLLATE pg_catalog."default",
+    "timestamp" character varying(255) COLLATE pg_catalog."default",
+    CONSTRAINT groupe_fkey FOREIGN KEY (groupe_fkey)
+        REFERENCES public.groupe (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT membre_fkey FOREIGN KEY (membre_fkey)
+        REFERENCES public.membre (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+-- Table: public.message_prive
+
+-- DROP TABLE public.message_prive;
+
+CREATE TABLE public.message_prive
+(
+    id SERIAL NOT NULL,
+    membre_envoyeur_fkey integer,
+    membre_receveur_fkey integer,
+    message_prive_contenu character varying(255) COLLATE pg_catalog."default",
+    "timestamp" character varying(255) COLLATE pg_catalog."default",
+    CONSTRAINT membre_envoyeur_fkey FOREIGN KEY (membre_envoyeur_fkey)
+        REFERENCES public.membre (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT membre_receveur_fkey FOREIGN KEY (membre_receveur_fkey)
+        REFERENCES public.membre (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+-- Table: public.message_prive_archive
+
+-- DROP TABLE public.message_prive_archive;
+
+CREATE TABLE public.message_prive_archive
+(
+    id SERIAL NOT NULL,
+    membre_envoyeur_fkey integer,
+    membre_receveur_fkey integer,
+    message_prive_contenu character varying(255) COLLATE pg_catalog."default",
+    "timestamp" character varying(255) COLLATE pg_catalog."default",
+    CONSTRAINT membre_envoyeur_fkey FOREIGN KEY (membre_envoyeur_fkey)
+        REFERENCES public.membre (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT membre_receveur_fkey FOREIGN KEY (membre_receveur_fkey)
+        REFERENCES public.membre (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+CREATE INDEX index_message_groupe_contenu ON message_groupe(message_groupe_contenu)
+CREATE INDEX index_message_groupe_fkey ON message_groupe(groupe_fkey)
+CREATE INDEX index_message_membre_fkey ON message_groupe(membre_fkey)
+
+CREATE INDEX index_message_membre_envoyeur_fkey ON message_prive(membre_envoyeur_fkey)
+CREATE INDEX index_message_membre_receveur_fkey ON message_prive(membre_receveur_fkey)
+CREATE INDEX index_message_prive_contenu ON message_prive(message_prive_contenu)
+
+CREATE VIEW nombre_msg_group AS SELECT COUNT(id) as nombre, groupe_fkey FROM public.message_groupe GROUP BY groupe_fkey;
